9cf297fecd4f8fad2661a1fc166181f117b2bb9f
lqw <382394323@qq.com>
2017-12-07 09:15:01 +0800

    Initial commit

---
+++ src/main/java/com/meiya/redis/RedisDemo1.java
@@ -0,0 +1,10 @@
+package com.meiya.redis;
+
+/**
+ * redis key-value存储系统，支持网络、可基于内存亦可持久化的日记型、key-value数据库
+ * 通常被称为数据结构服务器，因为值（value）可以是字符串（String）、哈希，列表（list）、
+ * 集合（sets）和有序集合（sorted sets）等类型
+ */
+
+public class RedisDemo1 {
+}
